219047f7b19ba4bea4da6b1780d51f132b668ff4
Opu Zaman <opu.zaman@shibumi.com>
2022-03-30 07:19:48 -0400

    fixed code

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -6,6 +6,6 @@ void call(){
     stage("SonarQube: Static Code Analysis"){
         
         println "static code analysis from the sonarqube library"
-        //  var1.build("php build")
+         var1.build("php build")
     }
 }
